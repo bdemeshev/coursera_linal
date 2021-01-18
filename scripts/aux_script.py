1d9c7c4c3e8e47c010b5e5a14b26f55dbfb5b761
Boris Demeshev <boris.demeshev@gmail.com>
2021-01-18 20:29:58 +0300

    3.1. misprint corrected

---
+++ scripts/aux_script.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+
+# week 03 
+A = np.array([[4, 3, 2], [0, 2, 8], [3, -1, 7]])
+A
+np.linalg.det(A)
+
+
+B = np.array([[4, 3, 2], [1, 2, 8], [3, -1, 7]])
+B
+np.linalg.det(B)
+
+np.math.factorial(8)
+
+# quiz 3.3
+A = np.array([[1, 2, -1], [1, 0, 2], [-2, 1, 3]])
+A
+np.linalg.inv(A)
+
+
+# week 05 
+
+# quiz 5.3
+
+A = np.array([[2, 3, 1], [3, 7, -1], [1, -1, 4]])
+A
+np.linalg.det(A)
